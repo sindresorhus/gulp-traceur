0abb7214a5603b8012d9cfdce3d2338e7c950301
meDavid <D.Kingma@jool.nl>
2014-04-30 13:23:52 +0200

    Fixed sourceMap caps and cs

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function (options) {
 			ret = traceur.compile(file.contents.toString(), options);
 
 			if (ret.js) {
-				file.contents = new Buffer(ret.js + (ret.SourceMap ? '\n//# sourceMappingURL='+ options.filename + '.map' : ''));
+				file.contents = new Buffer(ret.js + (ret.sourceMap ? '\n//# sourceMappingURL=' + options.filename + '.map' : ''));
 			}
 
 			if (ret.sourceMap) {
