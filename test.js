d86b5cf3081f649d4354ce5fb65735fbe4064af7
Damien Feugas <damien.feugas@worldline.com>
2014-08-04 13:17:14 +0200

    Close GH-22: Fix module names for "register" implementation. Fixes #15

--- test.js
+++ test.js
@@ -3,6 +3,24 @@ var assert = require('assert');
 var fs = require('fs');
 var gutil = require('gulp-util');
 var traceur = require('./index');
+var path = require('path');
+
+var fixtures = {
+	'fixture.js': 'import {Foo} from \'./foo\';',
+	'errored.js': 'cons x = 1;',
+	'calc.js': 'import {a, b} from "util/constants";\nimport {add} from "calc/add";\nconsole.log(add(a, b));',
+	'util/constants.js': 'export var a = 5;\nexport var b = 3;',
+	'calc/add.js': 'export function add (...args) {\n\treturn args.reduce((sum, val) => sum + val, 0);\n};'
+};
+
+function getFixtureFile (name) {
+	return new gutil.File({
+		cwd: __dirname,
+		base: __dirname + '/fixture' + name.substring(0, name.lastIndexOf('/')),
+		path: __dirname + '/fixture/' + name,
+		contents: new Buffer(fixtures[name])
+	})
+}
 
 it('should transpile with Traceur', function (cb) {
 	var stream = traceur({blockBinding: true});
@@ -12,12 +30,7 @@ it('should transpile with Traceur', function (cb) {
 		cb();
 	});
 
-	stream.write(new gutil.File({
-		cwd: __dirname,
-		base: __dirname + '/fixture',
-		path: __dirname + '/fixture/fixture.js',
-		contents: new Buffer('import {Foo} from \'./foo\';')
-	}));
+	stream.write(getFixtureFile('fixture.js'));
 });
 
 it('should pass syntax errors', function (cb) {
@@ -28,15 +41,70 @@ it('should pass syntax errors', function (cb) {
 		cb();
 	});
 
-	stream.write(new gutil.File({
-		cwd: __dirname,
-		base: __dirname + '/fixture',
-		path: __dirname + '/fixture/fixture.js',
-		contents: new Buffer('cons x = 1;')
-	}));
+	stream.write(getFixtureFile('errored.js'));
 });
 
 it('should expose the Traceur runtime path', function () {
 	assert(typeof traceur.RUNTIME_PATH === 'string');
 	assert(traceur.RUNTIME_PATH.length > 0);
+});
+
+it('should keep folder in module names with cjs modules', function (cb) {
+	// cjs is default module implementation
+	var stream = traceur();
+
+	stream.on('data', function (file) {
+		var content = file.contents.toString();
+		var name = path.relative(__dirname + '/fixture', file.path);
+		switch (name) {
+			case 'calc.js':
+				assert(/require\("util\/constants"\)/.test(content), 'calc.js does not require constants');
+				assert(/require\("calc\/add"\)/.test(content), 'calc.js does not require add');
+				break;
+			case path.join('util', 'constants.js'):
+			case path.join('calc', 'add.js'):
+				// just check that files have right names
+				break;
+			default:
+				cb('unexpected compiled file: ' + name);
+		}
+	});
+
+	stream.on('end', cb);
+
+	['calc.js', 'util/constants.js', 'calc/add.js'].forEach(function (name) {
+		stream.write(getFixtureFile(name));
+	});
+
+	stream.end();
+});
+
+it('should keep folder in module names with register modules', function (cb) {
+	var stream = traceur({modules: 'register', moduleName: true});
+
+	stream.on('data', function (file) {
+		var content = file.contents.toString();
+		var name = path.relative(__dirname + '/fixture', file.path);
+		switch (name) {
+			case 'calc.js':
+				assert(/System\.get\("util\/constants"\)/.test(content), 'calc.js does not require constants');
+				assert(/System\.get\("calc\/add"\)/.test(content), 'calc.js does not require add');
+				break;
+			case path.join('util', 'constants.js'):
+			case path.join('calc', 'add.js'):
+				var fileName = file.relative.replace(new RegExp('\\' + path.sep, 'g'), '/').replace('.js', '');
+				assert(new RegExp('System\\.register\\("' + fileName + '"').test(content), fileName + ' does not contains its filename');
+				break;
+			default:
+				cb('unexpected compiled file: ' + file.relative);
+		}
+	});
+
+	stream.on('end', cb);
+
+	['calc.js', 'util/constants.js', 'calc/add.js'].forEach(function (name) {
+		stream.write(getFixtureFile(name));
+	});
+
+	stream.end();
 });
