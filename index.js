31ad1e4256991b2f6b5e56aa49016256981b1f24
Curtis Schlak <realistschuckle@gmail.com>
2014-10-25 10:43:33 -0500

    Remove an extra catch block that didn't really need to exist.

--- index.js
+++ index.js
@@ -31,34 +31,27 @@ module.exports = function (options) {
 
 		try {
 			compiler = new traceur.Compiler(fileOptions);
+			contents = file.contents.toString();
+			ret = compiler.compile(contents, sourceName);
+			file.contents = new Buffer(ret);
 
-			try {
-				contents = file.contents.toString();
-				ret = compiler.compile(contents, sourceName);
-				file.contents = new Buffer(ret);
-
-				if (options.sourceMaps === true) {
-					source = new traceur.syntax.SourceFile(fileOptions.filename, contents);
-					parser = new traceur.syntax.Parser(source);
-					tree = parser.parseModule();
-					tree.moduleName = fileOptions.filename;
-					sourceMapConfig = {file: fileOptions.filename};
-					generator = new traceur.outputgeneration.SourceMapGenerator(sourceMapConfig);
-					sourceMapConfig = {sourceMapGenerator: generator};
-					traceur.outputgeneration.TreeWriter.write(tree, sourceMapConfig, fileOptions.filename);
-					applySourceMap(file, sourceMapConfig.generatedSourceMap);
-				}
-
-				cb(null, file);
-			} catch (e) {
-				cb(new gutil.PluginError('gulp-traceur', String(e), {
-					fileName: file.path,
-					showStack: false
-				}));
+			if (options.sourceMaps === true) {
+				source = new traceur.syntax.SourceFile(fileOptions.filename, contents);
+				parser = new traceur.syntax.Parser(source);
+				tree = parser.parseModule();
+				tree.moduleName = fileOptions.filename;
+				sourceMapConfig = {file: fileOptions.filename};
+				generator = new traceur.outputgeneration.SourceMapGenerator(sourceMapConfig);
+				sourceMapConfig = {sourceMapGenerator: generator};
+				traceur.outputgeneration.TreeWriter.write(tree, sourceMapConfig, fileOptions.filename);
+				applySourceMap(file, sourceMapConfig.generatedSourceMap);
 			}
-		} catch (err) {
-			cb(new gutil.PluginError('gulp-traceur', err, {
-				fileName: file.path
+
+			cb(null, file);
+		} catch (e) {
+			cb(new gutil.PluginError('gulp-traceur', String(e), {
+				fileName: file.path,
+				showStack: false
 			}));
 		}
 	});
